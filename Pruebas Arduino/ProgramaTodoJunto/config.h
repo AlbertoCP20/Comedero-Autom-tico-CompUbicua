82a8a1a86d603cf159a6b232640c1a6a3c59e015
AlbertoCP20 <79266546+AlbertoCP20@users.noreply.github.com>
2022-12-04 18:01:02 +0100

    Update config.h

--- Pruebas Arduino/ProgramaTodoJunto/config.h
+++ Pruebas Arduino/ProgramaTodoJunto/config.h
@@ -1,14 +1,15 @@
 //Wifi Settings
-const char* ssid	= "MOVISTAR_F096_EXT";
-const char* password = "4E2222449B7B34411B83";  const char* hostname = "ESP8266_1";
+const char* ssid	= "Alber_Movil";
+const char* password = "AlberMovil1234";  const char* hostname = "ESP8266_1";
 
 //MQTT Settings
-const char* MQTT_BROKER_ADRESS = "192.168.1.205";  const uint16_t MQTT_PORT = 1883;
+const char* MQTT_BROKER_ADRESS = "192.168.201.75";  const uint16_t MQTT_PORT = 1883;
 const char* MQTT_CLIENT_NAME = "ESP8266Client_1";
 
 //Server Settings
-IPAddress ip(192, 168, 1, 205);
-IPAddress gateway(192, 168, 1, 1);
+IPAddress ip(192, 168, 201, 75);
+IPAddress gateway(192, 168, 201, 65);
 IPAddress subnet(255, 255, 255, 0);
+
 
 
